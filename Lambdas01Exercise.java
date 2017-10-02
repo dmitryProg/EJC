d95e5ed469a30881902eeedf40efd6fcbb049058
Dmitry Gusev <dmitry.gusev.95@gmail.com>
2017-10-02 12:28:38 +0300

    Add files via upload

--- Lambdas01Exercise.java
+++ Lambdas01Exercise.java
@@ -1,1 +1,59 @@
+import com.google.common.collect.ImmutableList;
+import data.Person;
+import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class Lambdas01Exercise {
+    @Test
+    public void sortPersonsByAge() {
+        Person[] persons = {
+                new Person("name 3", "lastName 3", 20),
+                new Person("name 1", "lastName 2", 40),
+                new Person("name 2", "lastName 1", 30)
+        };
+
+        Arrays.sort(persons, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return Integer.valueOf(o1.getAge()).compareTo(o2.getAge());
+            }
+        });
+
+        assertArrayEquals(persons, new Person[]{
+                new Person("name 3", "lastName 3", 20),
+                new Person("name 2", "lastName 1", 30),
+                new Person("name 1", "lastName 2", 40),
+        });
+    }
+
+    @Test
+    public void findFirstWithAge30() {
+        List<Person> persons = ImmutableList.of(
+                new Person("name 3", "lastName 3", 20),
+                new Person("name 1", "lastName 2", 30),
+                new Person("name 2", "lastName 1", 30)
+        );
+
+        final Optional<Person> personOptional =
+                FluentIterable.from(persons)
+                        .firstMatch(new Predicate<Person>() {
+                            @Override
+                            public boolean apply(Person p) {
+                                return p.getAge() == 30;
+                            }
+                        });
+
+        if (personOptional.isPresent()) {
+            personOptional.get().print();
+        }
+
+        assertEquals(new Person("name 1", "lastName 2", 30), personOptional.get());
+    }
+}
